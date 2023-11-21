3bc03e90583583ed3c809bff2c9462b86e15cc31
Antoan Metodiev <Boksnem@abv.bg>
2023-11-21 12:32:50 +0200

    add task

---
+++ First Steps In Coding - Lab/InchesToCentimeters.js
@@ -0,0 +1,7 @@
+function printInchesToCentimeters(input) {
+    let inches = Number(input[0]);
+    let centimeters = inches * 2.54;
+    console.log(centimeters);
+}
+
+printInchesToCentimeters(["7"]);
