b7090f2901f1957473033c8382c6fbef6bc7f3d7
Antoan Metodiev <Boksnem@abv.bg>
2023-11-26 18:48:03 +0200

    add task

---
+++ Conditional Statements - Lab/SpeedInfo.js
@@ -0,0 +1,16 @@
+function speedInfo(input) {
+     let km = Number(input[0]);
+     if ((km > 0 || km <= 0) && km <= 10) {
+        console.log("slow");
+     } else if (km > 10 && km <= 50) {
+        console.log("average");
+     } else if (km > 50 && km <= 150) {
+        console.log("fast");
+     } else if (km > 150 && km <= 1000) {
+        console.log("ultra fast");
+     } else if (km > 1000) {
+        console.log("extremely fast");
+     }
+}
+
+speedInfo(["-27"]);
