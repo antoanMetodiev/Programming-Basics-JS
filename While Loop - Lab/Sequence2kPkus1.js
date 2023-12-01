e57c461a147542662610a0f22d4c0b0ba7383216
Antoan Metodiev <Boksnem@abv.bg>
2023-12-01 14:04:02 +0200

    add task

---
+++ While Loop - Lab/Sequence2kPkus1.js
@@ -0,0 +1,12 @@
+function sequence2kPkus1(input) {
+    let n = Number(input[0]);
+
+    let theNumber = 1;
+    while (theNumber <= n) {
+        
+        console.log(theNumber);
+        theNumber = (theNumber * 2) + 1;
+    }
+}
+
+sequence2kPkus1(["31"]);
