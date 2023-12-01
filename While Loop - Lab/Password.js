3434f71726f5d4d70e7fb76016d70b68ac558104
Antoan Metodiev <Boksnem@abv.bg>
2023-12-01 14:00:45 +0200

    add task

---
+++ While Loop - Lab/Password.js
@@ -0,0 +1,20 @@
+function password(input) {
+    let name = input[0];
+    let password = input[1];
+
+    let tryPassword = input[2];
+    let isTrue = false;
+    let indexCounter = 0;
+    while (!(tryPassword === password)) {
+        indexCounter++;
+        tryPassword	= input[indexCounter];
+    }
+
+    if (isTrue || tryPassword === password) {
+        console.log(`Welcome ${name}!`);
+    }
+}
+
+password(["Gosho",
+"secret",
+"secret"]);
