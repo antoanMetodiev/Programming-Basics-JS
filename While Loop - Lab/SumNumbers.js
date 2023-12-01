76fa2ae5b3e3e5c93e9b1b5515d45c3661dd6e0a
Antoan Metodiev <Boksnem@abv.bg>
2023-12-01 14:05:19 +0200

    add task

---
+++ While Loop - Lab/SumNumbers.js
@@ -0,0 +1,18 @@
+function sumNumbers(input) {
+    let theNumber = Number(input[0]);
+
+    let sum = 0;
+    let index = 0;
+    while (sum < theNumber) {
+        index++;
+        let num = Number(input[index]);
+        sum += num;
+    }
+    console.log(sum);
+}
+
+sumNumbers(["100",
+"10",
+"20",
+"30",
+"40"]);
