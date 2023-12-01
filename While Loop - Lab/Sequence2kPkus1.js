7d43be40fc824abe20f05c32619a7fe144d8218e
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2023-12-01 14:05:00 +0200

    Update Sequence2kPkus1.js

--- While Loop - Lab/Sequence2kPkus1.js
+++ While Loop - Lab/Sequence2kPkus1.js
@@ -1,9 +1,7 @@
 function sequence2kPkus1(input) {
     let n = Number(input[0]);
-
     let theNumber = 1;
     while (theNumber <= n) {
-        
         console.log(theNumber);
         theNumber = (theNumber * 2) + 1;
     }
