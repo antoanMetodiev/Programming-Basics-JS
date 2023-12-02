6a3791c3076eb21da90f4bd1752ecb9d3a5bdc8a
Antoan Metodiev <Boksnem@abv.bg>
2023-12-02 14:09:27 +0200

    add task

---
+++ While Loop - Lab/MinNumber.js
@@ -0,0 +1,20 @@
+function minNumber(input) {
+    let number = input[0];
+
+    let minNumber = 100000000000;
+    let index = 0;
+    while (!(number === "Stop")) {
+        number = Number(number);
+        if (number < minNumber) {
+            minNumber = number;
+        }
+        index++;
+        number = input[index];
+    }
+    console.log(minNumber);
+}
+
+minNumber(["-10",
+"20",
+"-30",
+"Stop"]);
